09e2b27d545c2a16cad0efd130c18a371527fd5d
Dongcai Huang <tcw6991@gmail.com>
2024-11-06 22:02:23 +0800

    Created a local development build eas-cli and sent it to my phone.
    The build command is:
    
    ```
    npx eas build --profile development --platform android --local
    ```
    
    Expected it displays audio data picked up from microphone on the screen, and it did.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,38 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { PermissionsAndroid, Text, View } from "react-native";
+import throttle from "lodash-es/throttle"
+import Recording from "react-native-recording"
 
 export default function Index() {
+  const [recordingData, setRecordingData] = useState<Float32Array>();
+
+  useEffect(() => {
+    let listener;
+    (async () => {
+      await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+
+      Recording.init({
+        bufferSize: 1024,
+        sampleRate: 44100,
+        bitsPerChannel: 16,
+        channelsPerFrame: 1,
+      });
+
+      listener = Recording.addRecordingEventListener(throttle((data) => {
+        setRecordingData(data.toString());
+      }, 1000));
+
+      Recording.start();
+    })();
+
+    return () => {
+      Recording.stop();
+      listener.remove();
+    }
+  }, []);
+
   return (
     <View
       style={{
@@ -9,7 +41,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{recordingData}</Text>
     </View>
   );
 }
